b96937167bbf0fb05a7794081dc033d262ee6bc4
Johan Sundell <johan@pixpro.net>
2016-11-24 15:07:19 +0100

    Trying to fix CORS

--- ip.go
+++ ip.go
@@ -25,6 +25,11 @@ func handleIpReq(w http.ResponseWriter, req *http.Request) {
 	ip := IpInfo{req.RemoteAddr}
 	ip.Clean()
 	b, err := json.Marshal(ip)
+
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+	w.Header().Add("Access-Control-Allow-Headers", "x-requested-with, Content-Type, origin, authorization, accept, client-security-token")
+
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		fmt.Println(err)
