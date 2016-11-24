55390d14e2b76b6db48b4d397dd86e3149ef684f
Johan Sundell <johan@pixpro.net>
2016-11-24 14:33:38 +0100

    Added json support to ip function

--- ip.go
+++ ip.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
+
+type IpInfo struct {
+	Ip string `json:"Ip"`
+}
+
+func (ip *IpInfo) Clean() {
+	if strings.Contains(ip.Ip, ":") {
+		ip.Ip = ip.Ip[:strings.Index(ip.Ip, ":")]
+	}
+}
 
 func init() {
 	routes = append(routes, Route{"ip", "GET", "/ip", handleIpReq})
 }
 
 func handleIpReq(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, req.RemoteAddr)
+	ip := IpInfo{req.RemoteAddr}
+	ip.Clean()
+	b, err := json.Marshal(ip)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprint(w, string(b))
 }
